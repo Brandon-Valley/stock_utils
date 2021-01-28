3a385f702243f17ced2240635da51622cce5fa66
Brandon Valley <bavvh8@mst.edu>
2021-01-27 18:29:42 -0600

    init gets past stock data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stock_utils
 Simple utilities for everything related to stocks, meant to be added as a submodule to actual projects.
+
+# Setup
+`pip install yfinance --user`
--- stock_utils.py
+++ stock_utils.py
@@ -1,1 +1,23 @@
-print("Hi")
+# print("Hi")
+# 
+# # import stock_info module from yahoo_fin
+# from yahoo_fin import stock_info as si
+# 
+# # get live price of Apple
+# si.get_live_price("aapl")
+#  
+# # or Amazon
+# si.get_live_price("amzn")
+#  
+# # # or any other ticker
+# # si.get_live_price(ticker)
+# 
+# print("done")
+
+
+import yfinance as yf
+
+
+# data = yf.download("ABEV3.SA", start="2020-03-01", end="2020-03-30")
+data = yf.download("ABR", start="2020-03-01", end="2020-03-30")
+print(data)
